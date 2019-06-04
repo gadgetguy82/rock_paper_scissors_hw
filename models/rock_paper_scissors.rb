16989dc1b9e89640d0f85e2d0fd787fbdddf7da0
Robert Tam <rsh.tam@gmail.com>
2019-06-04 20:00:26 +0100

    Add css styling and images

--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -5,20 +5,10 @@ class RockPaperScissors
   end
 
   def rock
-    if @throw == "scissors"
-      return "Player 1 wins by playing Rock"
-    elsif @throw == "paper"
+    if @throw == "paper"
       return "Player 2 wins by playing Paper"
-    else
-      return "a draw"
-    end
-  end
-
-  def scissors
-    if @throw == "rock"
-      return "Player 2 wins by playing Rock"
-    elsif @throw == "paper"
-      return "Player 1 wins by playing Scissors"
+    elsif @throw == "scissors"
+      return "Player 1 wins by playing Rock"
     else
       return "a draw"
     end
@@ -29,6 +19,16 @@ class RockPaperScissors
       return "Player 2 wins by playing Scissors"
     elsif @throw == "rock"
       return "Player 1 wins by playing Paper"
+    else
+      return "a draw"
+    end
+  end
+
+  def scissors
+    if @throw == "rock"
+      return "Player 2 wins by playing Rock"
+    elsif @throw == "paper"
+      return "Player 1 wins by playing Scissors"
     else
       return "a draw"
     end
